6629f549700e0e66253b45b8bcee5e949a82420e
dyun8080 <dyun8080@163.com>
2018-07-21 23:09:45 +0800

    feat: 自定义ejs模板

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,25 +1,48 @@
+/**
+ * 在默认的webpack配置上进行拓展
+ * See https://github.com/sorrycc/roadhog
+ * 
+ * ? ✔️ antd组件样式按需加载
+ * ? ✔️ 自定义主题色
+ * ? ✔️ 自定义html模板文件
+ * 
+ * 开发环境 development
+ * ? ✔️ 热启动
+ * 
+ * 生产环境 production
+ * ? ✔️ 静态文件hash处理
+*/
+
 export default {
   // babel 配置文件
   extraBabelPlugins: [
     [
-      // antd组件按需加载
+      // antd组件样式按需加载
       "import", {
         libraryName: "antd",
         libraryDirectory: "es",
-        // "css" or true，style：true时候加载less文件
+        // style："css" or true
+        // true时候加载less文件
         style: true
       }
     ]
   ],
+  // https://github.com/sorrycc/roadhog#extraresolveextensions
+  extraResolveExtensions: ['jsx'],
   // 配置主题色，其实是修改less变量，所以需要开启babel-plugin-import设置style：true 
   theme: {
     "@primary-color": "#1DA57A"
   },
-  hash: true,
-  // 不同环境单独配置
+  html: {
+    template: './src/index.ejs',
+  },
+  // 不同环境单独配置 'development' or 'production'
   env: {
     development: {
-      extraBabelPlugins: ["dva-hmr"]
+      extraBabelPlugins: ["dva-hmr"],
+    },
+    production: {
+      hash: true,
     }
   },
 }
