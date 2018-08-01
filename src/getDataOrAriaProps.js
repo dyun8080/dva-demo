8dc30fd29c96aadec2178d10cc04c1ae8f6a0f6c
dyun8080 <dyun8080@163.com>
2018-08-01 20:05:39 +0800

    test：测试一下jest测试

---
+++ src/getDataOrAriaProps.js
@@ -0,0 +1,13 @@
+function getDataOrAriaProps(props) {
+  return Object.keys(props).reduce((prev, key) => {
+    if (
+      (key.substr(0, 5) === 'data-' || key.substr(0, 5) === 'aria-' || key === 'role')
+      && key.substr(0, 7) !== 'data-__'
+    ) {
+      prev[key] = props[key];
+    }
+    return prev;
+  }, {});
+}
+
+module.exports = getDataOrAriaProps;
