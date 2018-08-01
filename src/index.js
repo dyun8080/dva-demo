e05b9f7b4828c88459d4fd740b32402075874294
dyun8080 <dyun8080@163.com>
2018-08-01 10:54:38 +0800

    feat: 初始化demo1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import dva from 'dva';
 import './index.less';
+import key from 'keymaster';
 
-// 1. Initialize
 const app = dva();
 
-// 2. Plugins
-// app.use({});
+const model = {
+  namespace: 'count',
+  state: {
+    record: 0,
+    current: 0,
+  },
 
-// 3. Model
-app.model(require('./models/example').default);
+  effects: {
+    *add(action, { call, put }) {
+      yield call(delay, 1000);
+      yield put({ type: 'minus' });
+    },
+
+    *wao(action, { call, put }) {
+      yield call(delay, 1000);
+      yield put({ type: 'minus' });
+    },
+  },
+
+  subscriptions: {
+    keyboardWatcher({ dispatch }) {
+      key('⌘+up, ctrl+up', () => { dispatch({ type: 'add' }); });
+    },
+  },
+
+  reducers: {
+    wao(state) {
+      const newCurrent = state.current + 100;
+      return {
+        ...state,
+        record: newCurrent > state.record ? newCurrent : state.record,
+        current: newCurrent,
+      };
+    },
+    add(state) {
+      const newCurrent = state.current + 1;
+      return {
+        ...state,
+        record: newCurrent > state.record ? newCurrent : state.record,
+        current: newCurrent,
+      };
+    },
+    minus(state) {
+      return { ...state, current: state.current - 1 };
+    },
+  },
+};
+
+
+app.model(model);
 
 // 4. Router
 app.router(require('./router').default);
 
 // 5. Start
 app.start('#root');
+
+function delay(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
--- src/routes/IndexPage.jsx
+++ src/routes/IndexPage.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'dva';
-import Button from '../components/Button';
+import { Button } from 'antd';
 import styles from './IndexPage.less';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>
-        Yay! Welcome to dva!
-      </h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-      <Button />
-    </div>
-  );
-}
-
-IndexPage.propTypes = {
+const Btn = ({ dispatch }) => {
+  return <Button onClick={() => dispatch({ type: 'count/wao' })}>一个神奇的按钮</Button>;
 };
 
-export default connect()(IndexPage);
+const ConnectBtn = connect()(Btn);
+
+
+@connect(({ count }) => ({ count }))
+class IndexPage extends Component {
+  render() {
+    const { count, dispatch } = this.props;
+    return (
+      <div className={styles.normal}>
+        <h1 className={styles.title}>
+          Yay! Welcome to dva!
+        </h1>
+        <div className={styles.welcome} />
+        <ul className={styles.list}>
+          <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+          <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
+        </ul>
+        <Button onClick={() => dispatch({ type: 'count/add' })}>+ 钮{count.current}</Button>
+        <Button onClick={() => dispatch({ type: 'count/minus' })}>- 按钮{count.current}</Button>
+        <div>最大值{count.record}</div>
+        <ConnectBtn />
+      </div>
+    );
+  }
+}
+
+
+export default IndexPage;
